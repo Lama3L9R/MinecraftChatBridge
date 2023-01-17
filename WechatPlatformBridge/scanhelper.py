017dd9f1513d1348377bc6e717816845a8fcb6c8
undy <52874216+HackingHackers@users.noreply.github.com>
2023-01-17 18:11:01 -0500

    Eliminate Pillow

--- WechatPlatformBridge/scanhelper.py
+++ WechatPlatformBridge/scanhelper.py
@@ -1,8 +1,10 @@
 import re
+import os
+import sys
 import base64
 import logging
 import requests
-from PIL import Image
+import subprocess
 
 logging.basicConfig(
     level=logging.INFO,
@@ -13,7 +15,7 @@ logging.basicConfig(
 MAGIC = "Go8FCIkFEokFCggwMDAwMDAwMRAGGvAESySibk50w5Wb3uTl2c2h64jVVrV7gNs06GFlWplHQbY/5FfiO++1yH4ykCyNPWKXmco+wfQzK5R98D3so7rJ5LmGFvBLjGceleySrc3SOf2Pc1gVehzJgODeS0lDL3/I/0S2SSE98YgKleq6Uqx6ndTy9yaL9qFxJL7eiA/R3SEfTaW1SBoSITIu+EEkXff+Pv8NHOk7N57rcGk1w0ZzRrQDkXTOXFN2iHYIzAAZPIOY45Lsh+A4slpgnDiaOvRtlQYCt97nmPLuTipOJ8Qc5pM7ZsOsAPPrCQL7nK0I7aPrFDF0q4ziUUKettzW8MrAaiVfmbD1/VkmLNVqqZVvBCtRblXb5FHmtS8FxnqCzYP4WFvz3T0TcrOqwLX1M/DQvcHaGGw0B0y4bZMs7lVScGBFxMj3vbFi2SRKbKhaitxHfYHAOAa0X7/MSS0RNAjdwoyGHeOepXOKY+h3iHeqCvgOH6LOifdHf/1aaZNwSkGotYnYScW8Yx63LnSwba7+hESrtPa/huRmB9KWvMCKbDThL/nne14hnL277EDCSocPu3rOSYjuB9gKSOdVmWsj9Dxb/iZIe+S6AiG29Esm+/eUacSba0k8wn5HhHg9d4tIcixrxveflc8vi2/wNQGVFNsGO6tB5WF0xf/plngOvQ1/ivGV/C1Qpdhzznh0ExAVJ6dwzNg7qIEBaw+BzTJTUuRcPk92Sn6QDn2Pu3mpONaEumacjW4w6ipPnPw+g2TfywJjeEcpSZaP4Q3YV5HG8D6UjWA4GSkBKculWpdCMadx0usMomsSS/74QgpYqcPkmamB4nVv1JxczYITIqItIKjD35IGKAUwAA=="
 
 
-def findBetween(str, start, end):
+def substrBetween(str, start, end):
     start = str.find(start)
     if start >= 0:
         start += len(start)
@@ -39,12 +41,16 @@ logging.debug("got uuid=" + uuid)
 
 rep = requests.get("https://login.wx.qq.com/qrcode/" + uuid + "?t=webwx")
 
-with open("tmp.jpg", "wb") as f:
+with open("login_qrcode.jpg", "wb") as f:
     f.write(rep.content)
     f.close()
 
-with Image.open("tmp.jpg") as img:
-    img.show()
+if sys.platform == "win32":
+    os.startfile("login_qrcode.jpg")
+elif sys.platform == "darwin":
+    subprocess.call(["open", "login_qrcode.jpg"])
+else:
+    subprocess.call(["xdg-open", "login_qrcode.jpg"])
 
 input("\nALERT: press ENTER after login is confirmed on mobile.")
 
@@ -70,10 +76,10 @@ rep = requests.get(
 
 logging.debug("login dataraw=" + rep.text)
 
-skey = findBetween(rep.text, "<skey>", "</skey>")
-wxsid = findBetween(rep.text, "<wxsid>", "</wxsid>")
-wxuin = findBetween(rep.text, "<wxuin>", "</wxuin>")
-pass_ticket = findBetween(rep.text, "<pass_ticket>", "</pass_ticket>")
+skey = substrBetween(rep.text, "<skey>", "</skey>")
+wxsid = substrBetween(rep.text, "<wxsid>", "</wxsid>")
+wxuin = substrBetween(rep.text, "<wxuin>", "</wxuin>")
+pass_ticket = substrBetween(rep.text, "<pass_ticket>", "</pass_ticket>")
 cookie = rep.headers.get("set-cookie")
 
 print(
